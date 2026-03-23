3d75147c40a08d7e3247a9d2d8859f277608f40e
Nicholas <danielnick210@gmail.com>
2026-03-23 10:07:37 +0100

    updated the conditions

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello World");
-	console.log(req.url)
-	// something goes her
+
+	if (req.url === "/") {
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.end("This is the home page")
+	} else if (req.url === "/about") {
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.end("This is the about page");
+	} else {
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.end("Page not found");
+	}
 });
 
 
