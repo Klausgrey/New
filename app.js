b199d40895c49a5d321c4757948f80d733b3ce45
Nicholas <danielnick210@gmail.com>
2026-03-24 22:13:58 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const Database = require("better-sqlite3")
-const db = new Database("notes.db")
+const Database = require("better-sqlite3");
+const db = new Database("notes.db");
 
-db.prepare(`
+db.prepare(
+	`
 	CREATE TABLE IF NOT EXISTS notes (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		title TEXT NOT NULL,
 		content TEXT NOT NULL
 	)
-`).run();
+`,
+).run();
 
+app.get("/notes", function (req, res) {
+	const result = db.prepare("SELECT * FROM notes").all();
+	res.json(result);
+});
+
+app.post("/notes", function (req, res) {
+	const title = req.body.title
+	const content = req.body.content
+
+	db.prepare("INSERT INTO notes (title, content) VALUES (?, ?)").run(title, content)
+	res.json(`Your ${title} has been created successfully`)
+});
 
 app.listen(3000, () => {
-	console.log("The server is running xoxo")
-})
+	console.log("The server is running xoxo");
+});
