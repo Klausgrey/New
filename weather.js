9f40efb3c3b0560a06ff23648aaa7a0041efdf5a
Nicholas <danielnick210@gmail.com>
2026-03-26 09:21:21 +0100

    working with external apis, fetched the weather api to tell the weather of a particular city

---
+++ weather.js
@@ -0,0 +1,20 @@
+const nameGrab = process.argv[2];
+require("dotenv").config();
+const key = process.env.apiKey;
+
+async function getWeather(nameGrab) {
+	const response = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${nameGrab},&APPID=${key}&units=metric`,
+	);
+	const data = await response.json();
+	console.log(`
+		City: ${data.name}
+		Temperature: ${data.main.temp}
+		Condition: ${data.weather[0].description}
+		Humidity: ${data.main.humidity}
+		Wind: ${data.wind.speed}
+
+		`);
+}
+
+getWeather("Uyo");
