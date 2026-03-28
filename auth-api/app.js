e6fc830504a2808d14834328e32e1d5d4fc4f42c
Nicholas <danielnick210@gmail.com>
2026-03-28 01:44:38 +0100

    auth users and checked for tokens

--- auth-api/app.js
+++ auth-api/app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const Database = require("better-sqlite3");
 const db = new Database("auth.db");
 const JWT_SECRET = process.env.JWT_SECRET;
+const PORT = process.env.PORT
 
 const app = express();
 app.use(express.json());
@@ -54,6 +55,29 @@ app.post("/login", async (req, res) => {
 	res.json({ token });
 });
 
-app.listen(3000, () => {
+const verifyToken = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).json({ message: "No token provided" });
+	}
+	const token = authHeader.split(" ")[1];
+
+	if (!token) {
+		res.status(400).json({ message: "Bad request" });
+	}
+	next();
+	try {
+		const decoded = jwt.verify(token, JWT_SECRET);
+		req.user = decoded;
+	} catch (err) {
+		res.status(401).json({ message: "Invalid token" });
+	}
+};
+
+app.get("/protected", verifyToken, async (req, res) => {
+	res.json({ message: "You have access" });
+});
+
+app.listen(PORT, () => {
 	console.log("Running");
 });
