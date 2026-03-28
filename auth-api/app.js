6af33fcfb64ff633f29d37568b7c0412e2da61c2
Nicholas <danielnick210@gmail.com>
2026-03-28 01:52:38 +0100

    bug fix

--- auth-api/app.js
+++ auth-api/app.js
@@ -65,10 +65,9 @@ const verifyToken = (req, res, next) => {
 	if (!token) {
 		res.status(400).json({ message: "Bad request" });
 	}
-	next();
 	try {
-		const decoded = jwt.verify(token, JWT_SECRET);
-		req.user = decoded;
+		jwt.verify(token, JWT_SECRET);
+		next();
 	} catch (err) {
 		res.status(401).json({ message: "Invalid token" });
 	}
